e803b8b28287ddd9680ac0e34836d09f3f3f0357
Jorge Barrera <jorge@teaspeak.local>
2025-12-30 04:53:16 +0000

    Fix: Detect zstd CMakeLists.txt location automatically
    - New zstd versions have CMakeLists.txt in root, not build/cmake/
    - Auto-detect directory to support both old and new versions
    - Fixes 'Missing target directory' error

--- libraries/build_zstd.sh
+++ libraries/build_zstd.sh
@@ -18,6 +18,11 @@ _fpic=""
 [[ ${build_os_type} == "linux" ]] && _fpic="-fPIC"
 _std_options=""
 [[ ${build_os_type} == "linux" ]] && _std_options="-static-libstdc++"
-cmake_build ${library_path}/build/cmake/ -DZSTD_BUILD_PROGRAMS=OFF -DCMAKE_CXX_FLAGS="${_fpic} ${_std_options}" -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+# Detectar ubicación de CMakeLists.txt (versiones nuevas en raíz, viejas en build/cmake)
+_cmake_path="${library_path}"
+[[ -d "${library_path}/build/cmake" ]] && _cmake_path="${library_path}/build/cmake"
+
+cmake_build ${_cmake_path} -DZSTD_BUILD_PROGRAMS=OFF -DCMAKE_CXX_FLAGS="${_fpic} ${_std_options}" -DCMAKE_BUILD_TYPE=RelWithDebInfo
 check_err_exit ${library_path} "Failed to build zstd!"
 set_build_successful ${library_path}
